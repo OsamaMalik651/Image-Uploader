57749e010997858508ff03bee90a8dbf7f497ee7
OsamaMalik651 <osamamalik124@outlook.com>
2022-09-05 13:51:00 -0600

    api fixes

--- client/src/services/UploadImage.js
+++ client/src/services/UploadImage.js
@@ -5,7 +5,7 @@ const uploadImage = async (selectedImage) => {
     // try catch
     const image = new FormData();
     image.append("image", selectedImage)
-    const response = await axios.post("http://localhost:5000/api/image-upload", image)
+    const response = await axios.post("/api/image-upload", image)
     return response.data
 }
 
